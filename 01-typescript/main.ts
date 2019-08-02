db34aa21f86b668960c96fc279547ea5474418b3
diego@tognola.net <1kIvxWw6P4v#>
2019-08-02 19:36:55 +1000

    some typescript examples

---
+++ 01-typescript/main.ts
@@ -0,0 +1,47 @@
+// needed to define local scope
+export { }
+
+let greeting: string = "Hello world"
+console.log(greeting)
+
+// interfaces
+interface toy {
+    name: string
+    minAge: number
+    play(): void
+}
+
+let marbles: toy = {
+    name: "marbles",
+    minAge: 3,
+    play: () => console.log("Playing marbles")
+}
+console.log("Playing:")
+marbles.play()
+
+// classes
+class toyshop {
+    address: string
+
+    constructor(address: string) {
+        this.address = address
+    }
+
+    sell(item: toy) {
+        let message = `selling ${item.name} (minimum age ${item.minAge})`
+        console.log("Message=" + message)
+    }
+}
+
+let toysAreMe = new toyshop("33 Main Street")
+toysAreMe.sell(marbles)
+
+// generics
+function jsonCleaner<T>(input: T): T {
+    const serialised = JSON.stringify(input)
+    const deserialised = JSON.parse(serialised)
+    return deserialised
+}
+
+console.log("Cleaned: " + jsonCleaner<string>("Hi"))
+console.log("Cleaned: " + jsonCleaner<number>(33))

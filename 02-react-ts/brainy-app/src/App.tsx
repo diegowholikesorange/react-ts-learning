3ab68326a964d25f431b689465769a9f1699fe01
diego@tognola.net <1kIvxWw6P4v#>
2019-08-05 14:42:21 +1000

    adding first input property

--- 02-react-ts/brainy-app/src/App.tsx
+++ 02-react-ts/brainy-app/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FunctionComponent = () => {
+interface Props {
+  title: string
+}
+
+const App: React.FunctionComponent<Props> = (props) => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Hello
+          Hello {props.title}
         </p>
       </header>
     </div>
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Learning React &amp; TypeScript
 
 - Creating React-TS project: https://facebook.github.io/create-react-app/docs/adding-typescript
 - React-TS and Hooks: https://levelup.gitconnected.com/usetypescript-a-complete-guide-to-react-hooks-and-typescript-db1858d1fb9c
-
+- https://alligator.io/react/typescript-with-react/
 

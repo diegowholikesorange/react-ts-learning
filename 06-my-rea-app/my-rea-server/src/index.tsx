9cf5d62446004ea9c5d645751f438b22a0a2281e
diego@tognola.net <1kIvxWw6P4v#>
2019-11-19 20:17:47 +1100

    moving out resolvers into own module

--- 06-my-rea-app/my-rea-server/src/index.tsx
+++ 06-my-rea-app/my-rea-server/src/index.tsx
@@ -2,9 +2,8 @@ import {ApolloServer} from 'apollo-server';
 import {importSchema} from 'graphql-import'
 import * as queryResolvers from "./resolvers";
 
-const typeDefs = importSchema('./src/schema.graphql')
-const resolvers = queryResolvers.resolvers;
-const server = new ApolloServer({resolvers, typeDefs});
+const schema = importSchema('./src/schema.graphql')
+const server = new ApolloServer({resolvers: queryResolvers.resolvers, typeDefs: schema});
 
 server.listen()
     .then(({url}) => console.log(`Server ready at ${url}. `));

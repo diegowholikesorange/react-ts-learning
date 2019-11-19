0536ea00fd64e186161b06ff72d721a28682a8d1
Diego Tognola <22850806+diegowholikesorange@users.noreply.github.com>
2019-11-19 23:30:33 +1100

    add icons to list

--- 06-my-rea-app/my-rea-client/src/components/HelloWorld/HelloWorld.tsx
+++ 06-my-rea-app/my-rea-client/src/components/HelloWorld/HelloWorld.tsx
@@ -2,23 +2,49 @@ import * as React from 'react';
 import {HelloQuery} from '../../generated/graphql';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import HouseIcon from '@material-ui/icons/House';
+
 
 interface Props {
     data: HelloQuery;
 }
 
-const HelloWorld: React.FC<Props> = ({data}) => (
-    <div>
-        {data.pageContent!.welcome}
-        <List component="nav">
-            {data.pageContent!.properties!.map((ithProperty) =>
-                <ListItem>
-                    {ithProperty!.address}
-                </ListItem>
-            )}
-        </List>
-
-    </div>
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            backgroundColor: theme.palette.background.paper,
+        },
+    }),
 );
+
+
+const HelloWorld: React.FC<Props> = ({data}) => {
+
+    const styleClasses = useStyles();
+
+    return (
+        <div>
+            {data.pageContent!.welcome}
+            <List className={styleClasses.root}>
+                {data.pageContent!.properties!.map((ithProperty) =>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <HouseIcon/>
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={ithProperty!.address}/>
+                    </ListItem>
+                )}
+            </List>
+        </div>
+    )
+};
 
 export default HelloWorld;

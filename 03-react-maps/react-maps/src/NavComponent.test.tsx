7b44ef3bb63066203f08bee92efe6be423a3d8f9
diego@tognola.net <1kIvxWw6P4v#>
2020-01-28 22:33:16 +1100

    add test to validate that the reset button updates the scale stored in the MapsContext

--- 03-react-maps/react-maps/src/NavComponent.test.tsx
+++ 03-react-maps/react-maps/src/NavComponent.test.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
-
 import {cleanup, fireEvent, render} from "@testing-library/react";
-import App from "./App";
+import NavComponent from "./NavComponent";
+import React from "react";
+import {MapsContext, MyAppContextProvider} from "./MyAppContext";
 
 // Arrange
 afterEach(cleanup);
 
-it('shows a button labelled submit', () => {
+it('shows a button labelled reset', () => {
 
     // Arrange
-    const page = render(<App/>);
+    const page = render(<NavComponent/>);
     const resetButton = page.getByText(`Reset`);
 
     // Assert
     expect(resetButton).toBeDefined();
+
+});
+
+
+
+it('clicking the reset button should set scale in context to 42', () => {
+
+    // mock the tightly coupled Context with a Context that is not using the state hook and therefore is accessible in this test
+    let mockMapsContext: MapsContext = {
+        scale: 0,
+        scaleSetter: (setStateAction) => {
+            mockMapsContext.scale = +setStateAction.toString(); // a complete hack to get the argument out of the Dispatch<SetStateAction<number>>
+        }
+    };
+
+    // Arrange
+    const page = render(
+        <MyAppContextProvider value={mockMapsContext}>
+            <NavComponent/>
+        </MyAppContextProvider>
+    );
+    const resetButton = page.getByText(`Reset`);
+
+    // Act
+    fireEvent.click(resetButton);
+
+    // Assert
+    expect(mockMapsContext.scale).toEqual(42);
+
 });
